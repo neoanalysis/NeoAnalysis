291e6afa60a74575ba7ce027c8c6a3b9385f1629
neoanalysis <neoanalysis@163.com>
2017-10-02 01:44:09 +0800

    delete

--- NeoAnalysis_Py2.7/NeoAnalysis/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = '0.10.0'
-from NeoAnalysis import TransFile
-from NeoAnalysis.spikedetection import SpikeDetection
-from NeoAnalysis.spikesorting import SpikeSorting
-from NeoAnalysis.analogfilter import AnalogFilter
-from NeoAnalysis.graphics import Graphics
-from NeoAnalysis.popuanalysis import PopuAnalysis
--- NeoAnalysis_Py3.5/NeoAnalysis/neo/test/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-The subdirectory coretest contains tests for neo.core
-
-The subdirectory iotest contains tests for neo.io
-"""
